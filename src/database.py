02436582b41764086fa04b27e642969466b7ddca
robot-le <fake@mail.com>
2024-07-21 20:40:19 +0600

    Add async alembic
    - Initializes alembic
    - Edit alembic configuration, so it could work in asynchronous mode
    - Removes database initialization due to uselessness since we use alembic

--- src/database.py
+++ src/database.py
@@ -1,8 +1,6 @@
-from sqlmodel import create_engine, SQLModel, Session
 from src.config import settings
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import sessionmaker
 
 
 engine = create_async_engine(
@@ -11,19 +9,6 @@ engine = create_async_engine(
     future=True,
 )
 
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-
-# async def get_session():
-#     session = AsyncSession(engine)
-#     try:
-#         yield session
-#     finally:
-#         await session.close()
 
 async def get_session() -> AsyncSession:
     async_session = async_sessionmaker(
